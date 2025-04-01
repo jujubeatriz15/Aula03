96e94acf26269fa688773f4e5be21c9cff55399c
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-01 09:21:30 -0300

    atualizacao da atividade anterior

--- exercicio01.py
+++ exercicio01.py
@@ -1,4 +1,9 @@
+#atividade do dia 01/04
 nome = input("digite seu nome ")
 idade = int(input("digite sua idade "))
-salário = float((input("digite seu salario "))
-print(f"seu nome é {nome} um belo nome, você tem {idade} anos, seu salário é {salário} reais")
+salario = float(input("qual o seu salario: "))
+aumento = float(input("qual foi o percentual do seu aumento: "))
+valorReal = salario*aumento/100
+novoSalario = salario+valorReal
+print(f"Ola {nome} sua idade é {idade} e seu salario é {salario}")
+print(f"Seu aumento foi {novoSalario}")
