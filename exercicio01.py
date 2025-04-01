3b4c83069ef8515b7a23c24d5ced6cc58d627076
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-01 08:50:33 -0300

    fiz a primeira tarefa do dia na aula

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+nome = input("digite seu nome ")
+idade = int(input("digite sua idade "))
+salário = float((input("digite seu salario "))
+print(f"seu nome é {nome} um belo nome, você tem {idade} anos, seu salário é {salário} reais")
