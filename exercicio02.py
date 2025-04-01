352298174797059df72f4a3c2410adc0f86e2fcc
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-01 09:41:04 -0300

    exercicio 02 feito

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+#exercicio02 das 4 operacoes
+numero01 = float(input("digite o primeiro numero: "))
+numero02 = float(input("digite o segundo numero: "))
+adicao = numero01+numero02
+subtracao = numero01-numero02
+multiplicao = numero01*numero02
+divisao = numero01/numero02
+print(f"{numero01} + {numero02} = {adicao}")
+print(f"{numero01} - {numero02} = {subtracao}")
+print(f"{numero01} * {numero02} = {multiplicao}")
+print(f"{numero01} / {numero02} = {divisao}")
