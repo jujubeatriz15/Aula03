21002f29940756b1fb5233c404c7b71e69e86303
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-03 09:33:40 -0300

    exercicio08 feito

---
+++ exercicio08.py
@@ -0,0 +1,7 @@
+#exercicio08
+numero = int(input("digite um numero pra saber se é par ou impar: "))
+if numero % 2 == 0:
+    print(f"esse {numero} é PAR")
+
+else:
+    print(f"esse número {numero} é ÍMPAR")
