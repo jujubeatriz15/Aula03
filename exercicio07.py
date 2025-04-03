249b61411f0194e847940166d076f9877440cf71
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-03 09:02:07 -0300

    exercicio07 feito: sobre descobrir o preço da gasolina ou etanol em litros de acordo com preço de cadaa um deles

--- exercicio07.py
+++ exercicio07.py
@@ -1,13 +1,12 @@
 #exercicio07
 comb =  input("digite o tipo de combustivel:\n"
-              "G pra gasolina\n "
-              "E pra etanol\n ")
-litros = float(input("qual o litro: "))
+              "G ou g pra gasolina\n "
+              "E ou e pra etanol\n ")
+litros = float(input("digite aqui o litro: "))
 Vgas = 5.8
 Veta = 4.9
 if comb == "G":
     valor = litros * Vgas
 else:
     valor = litros * Veta
-
 print(f"você gastou em R${valor:.2f}")
