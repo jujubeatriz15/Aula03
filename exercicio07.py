e10e1b4f8b9d37765673bc34670b6e3bfc2cac45
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-03 09:18:58 -0300

    exercicio07 feito

--- exercicio07.py
+++ exercicio07.py
@@ -1,12 +1,15 @@
 #exercicio07
 comb =  input("digite o tipo de combustivel:\n"
-              "G ou g pra gasolina\n "
-              "E ou e pra etanol\n ")
+              "G pra gasolina\n "
+              "E pra etanol\n ")
 litros = float(input("digite aqui o litro: "))
 Vgas = 5.8
 Veta = 4.9
-if comb == "G":
+if comb == "G" or comb == "g":
     valor = litros * Vgas
-else:
+
+elif comb == "E" or comb == "e":
     valor = litros * Veta
+else:
+    print("tipo de combustível invalido!")
 print(f"você gastou em R${valor:.2f}")
